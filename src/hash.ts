4e3c97db96065e02b0727196504c8993bc7ae865
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-02-01 04:20:41 +0800

    use urn format to specify hash algorithm used

--- src/hash.ts
+++ src/hash.ts
@@ -1,36 +1,38 @@
 import crypto from 'crypto'
 
-function createHash() {
-  return crypto.createHash('sha256')
+class Hash {
+  stream = crypto.createHash('sha256')
+  write(chunk: string) {
+    this.stream.write(chunk)
+  }
+  digest() {
+    return 'urn:sha256:' + this.stream.digest().toString('hex')
+  }
 }
 
 function encodeInt(timestamp: number): string {
   return timestamp.toString(36)
 }
 
-function encodeHash(hash: crypto.Hash): string {
-  return hash.digest().toString('hex')
-}
-
 export function hashTxn(input: { payload: string; timestamp: number }): string {
-  let hash = createHash()
+  let hash = new Hash()
   hash.write(encodeInt(input.timestamp) + ':')
   hash.write(input.payload)
-  return encodeHash(hash)
+  return hash.digest()
 }
 
 export function encodeBlockContent(txn_hashes: string[]): {
   hash: string
   payload: string
 } {
-  let hash = createHash()
+  let hash = new Hash()
   let payload = ''
   for (let txn_hash of txn_hashes) {
     let chunk = txn_hash + ':'
     hash.write(chunk)
     payload += chunk
   }
-  return { hash: encodeHash(hash), payload }
+  return { hash: hash.digest(), payload }
 }
 
 export function hashBlockHeader(input: {
@@ -39,10 +41,10 @@ export function hashBlockHeader(input: {
   nonce: number
   timestamp: number
 }): string {
-  let hash = createHash()
+  let hash = new Hash()
   hash.write(input.prev_block_hash + ':')
   hash.write(input.content_hash + ':')
   hash.write(encodeInt(input.nonce) + ':')
   hash.write(encodeInt(input.timestamp) + ':')
-  return encodeHash(hash)
+  return hash.digest()
 }
